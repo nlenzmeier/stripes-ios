dbf76271f4a3e02d94ebccd5d63daeb165957100
Nicolle Lenzmeier <nalyv2@mail.missouri.edu>
2018-08-09 22:29:28 -0500

    Initial Commit

---
+++ STRIPES/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  STRIPES
+//
+//  Created by Nicolle on 8/9/18.
+//  Copyright © 2018 Nicolle Lenzmeier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
