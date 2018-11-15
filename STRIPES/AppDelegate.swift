2fdca315a9a524ed4632b556087aa75304a4f1ad
Nicolle Lenzmeier <nalyv2@mail.missouri.edu>
2018-11-14 23:40:03 -0600

    Create EstimatedWaitTime wrapper

--- STRIPES/AppDelegate.swift
+++ STRIPES/AppDelegate.swift
@@ -13,6 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    let rideStatus = RideStatus()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // set navigation background color
---
+++ STRIPES/RideStatus.swift
@@ -0,0 +1,13 @@
+//
+//  RideStatus.swift
+//  STRIPES
+//
+//  Created by Nicolle on 11/14/18.
+//  Copyright © 2018 Nicolle Lenzmeier. All rights reserved.
+//
+
+import Foundation
+
+class RideStatus {
+    var state = EstimatedWaitTime.initial
+}
