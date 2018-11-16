cd844d1ee3066f6b47032851307f12db0bb7c6ec
Nicolle Lenzmeier <nalyv2@mail.missouri.edu>
2018-11-15 23:10:00 -0600

    Change word wrap on text label

--- STRIPES/RideStatus.swift
+++ STRIPES/RideStatus.swift
@@ -9,7 +9,18 @@
 import Foundation
 
 class RideStatus {
-    var state = EstimatedWaitTime.initial
+    var state = EstimatedWaitTime.initial {
+        willSet {
+            NSLog("I'm about to be changed.")
+        }
+        didSet {
+            NSLog("I've been changed.")
+            
+            // let those that listen to me know of my achievement! AKA: broadcast notification
+            let center = NotificationCenter.default
+            center.post(name: RideStatus.rideStatusChange, object: nil)
+        }
+    }
     
     var dataTask: URLSessionDataTask?
     
@@ -18,21 +29,14 @@ class RideStatus {
     
     func retrieveRideStatus() {
         let url = URL(string: "http://104.248.54.97/api/WaitTime")!
-        // let url = URL(string: "http://127.0.0.1:3000/notRunning")!
+        //let url = URL(string: "http://127.0.0.1:3000/time")!
         
         let session = URLSession.shared
         let dataTask = session.dataTask(with: url) { data, urlResponse, error in
             if let data = data, let string = String(data: data, encoding: .utf8) {
                 print(string)
                 
-                self.state = EstimatedWaitTime.init(data: data)
-                NSLog("I've been modified!")
-            
-                // boradcast notification
-                let center = NotificationCenter.default
-                center.post(name: RideStatus.rideStatusChange, object: nil)
-                
-                
+                self.state = EstimatedWaitTime.init(data: data)            
             }
         }
         self.dataTask = dataTask
